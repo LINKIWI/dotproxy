bec0b0a1d7b13c25e104e3ac316d6f9b30a7dc37
Kevin Lin <developer@kevinlin.info>
2019-03-02 20:50:18 -0800

    internal/protocol: Package documentation

---
+++ internal/protocol/doc.go
@@ -0,0 +1,4 @@
+// Package protocol concerns itself primarily with DNS protocol-specific business logic. It contains
+// logic used for understanding the DNS protocol and mediating requests and responses between the
+// client and upstream server(s).
+package protocol
