e1afbf2a0954dcef66608096875df2a24bb7bbb3
Kevin Lin <developer@kevinlin.info>
2019-03-02 20:50:12 -0800

    internal/network: Package documentation

---
+++ internal/network/doc.go
@@ -0,0 +1,4 @@
+// Package network contains abstractions for communicating with other machines over the network. It
+// attempts to abstract away the API differences between TCP and UDP connections, and minimizes the
+// exposed interaction surfaces for TLS connections in an effort to simplify client usage.
+package network
